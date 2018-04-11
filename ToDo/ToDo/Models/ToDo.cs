d13e61102c3005fda4d5b99b7b26144fdb433347
Playingtygre <kymiehsu123@gmail.com>
2018-04-11 09:23:56 -0700

    Adding models

---
+++ ToDo/ToDo/Models/ToDo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public bool IsComplete { get; set; }
+
+
+        //Links back to The Models.TodoList
+        public ToDoList ToDoList { get; set;}
+    }
+}
---
+++ ToDo/ToDo/Models/ToDoList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+
+        //Links back to the models.todo
+        public ToDo ToDo { get; set; }
+    }
+}
