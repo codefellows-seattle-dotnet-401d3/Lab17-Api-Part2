27ad5aca122a057b1ffda5fd6158bcea73172f20
Playingtygre <kymiehsu123@gmail.com>
2018-04-15 15:21:55 -0700

    ASI

---
+++ TodoAPI/TodoAPI/Models/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoAPI.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        public string Message { get; set; }
+
+        public bool IsDone { get; set; }
+
+        public int? ListId { get; set; }
+
+        public TodoList List {get;set; }
+
+    }
+}
